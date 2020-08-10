e7a876add081a4cfda937295cadc8e7de9b2d19d
myp <741620315@qq.com>
2020-08-10 10:15:25 +0800

    注册中心更改为nacos，增加服务网关模块

--- eureka-client-admin/src/main/java/com/myp/cloud/eurekaclientadmin/Admin.java
+++ eureka-client-admin/src/main/java/com/myp/cloud/eurekaclientadmin/Admin.java
@@ -4,9 +4,11 @@ import com.myp.cloud.feigncommoninterface.interfaces.UserService;
 import com.myp.cloud.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("v1/seller")
 public class Admin{
     @Autowired
     UserService userService;
